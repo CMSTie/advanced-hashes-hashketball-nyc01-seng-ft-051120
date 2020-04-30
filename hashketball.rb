d7cfc8aae1081eec3c699644ae439664a7212a62
Chris Hosler <cmstie@gmail.com>
2020-04-30 18:48:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,7 @@ def player_numbers(desired_team_name)
       team_data.each do |stats, value|
       if stats == :players then
         value.each do |player_data|
-        points.push(player_data[:number])
+        numbers.push(player_data[:number])
       end
       end
       end
