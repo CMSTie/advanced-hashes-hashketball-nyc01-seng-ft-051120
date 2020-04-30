df66ce85bea1a6bd5ac3cad1ed0085da71d76449
Chris Hosler <cmstie@gmail.com>
2020-04-30 19:48:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,6 +203,7 @@ def big_shoe_rebounds()
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
       if player[:shoe] > player_big_shoe
+        player_big_shoe = player[:shoe]
         player_rebound = player[:rebounds]
       end
 end
