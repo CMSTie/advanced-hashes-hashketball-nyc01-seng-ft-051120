52c4790c7820990550b93dcd79ac9add78930799
Chris Hosler <cmstie@gmail.com>
2020-04-30 18:13:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,8 +156,7 @@ end
 
 def team_colors(team)
   game_hash.each do |location, team_data|
-    team_data.each do |data|
-    if data[:team_name] == team
+    if team_datadata[:team_name] == team
       return data[:colors]
     end
     end
